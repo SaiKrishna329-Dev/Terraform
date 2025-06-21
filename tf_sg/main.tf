95a1fe67e69c65f795a6b936fa1a113f89d6ca12
SaiKrishna329-Dev <gsaikrishna1496@gmail.com>
2025-06-21 09:50:37 +0530

    ec2-workspace

---
+++ tf_sg/main.tf
@@ -0,0 +1,42 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+
+# To create sg, VPC must needed if we are not provided the VPC it will take the default VPC id for sg creation.
+
+resource "aws_security_group" "tf_sg" {
+  name        = "example-sg"
+  description = "Allow traffic"
+
+    tags = {
+    Name = "allow_tls"
+  }
+  
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" # semantically equivalent to all ports
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
